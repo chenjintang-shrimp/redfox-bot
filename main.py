3074972347398167dd0ed13ffdc84ff780bf5d1f
jimmy-sketch <jimmy163mail@163.com>
2026-01-16 22:28:59 +0800

    feat: Add extensive user data models, user-related commands, and a module reload command.

--- main.py
+++ main.py
@@ -13,4 +13,9 @@ async def on_ready():
     await bot.tree.sync(guild=discord.Object(id=1404817877504229426))
     logging.getLogger("discord").info("Ready.")
 
+@bot.command(name="reload")
+async def reload_module(ctx: commands.Context,name: str):
+    await bot.reload_extension(f"commands.{name}")
+    await ctx.send(f"Module {name} reloaded")
+
 bot.run("REMOVE")
